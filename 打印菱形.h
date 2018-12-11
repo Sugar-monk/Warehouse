26b48ab4b1c4cd894e0c6d0bc3c6c6190c50b626
suger-monk <1835868676@qq.com>
2018-12-11 12:38:54 +0800

    打印n行菱形，并且每行之间相差2个，最多一行为n个

---
+++ 打印菱形.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include<stdio.h>
+
+void Print(size_t n)   //n为要打印的行数
+{
+	int i = 0;
+	for (i = 0; i <= n; i+=2)
+	{
+		for (size_t j = 0; j < i +1 ; ++j)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (i = n - 2; i >0; i -= 2)
+	{
+		for (size_t j = 0; j < i ; ++j)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+}
