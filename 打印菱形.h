bf9eb159aa73a64ab3628525fe1622288c8109b4
suger-monk <1835868676@qq.com>
2018-12-11 12:53:12 +0800

    打印菱形优化版

--- 打印菱形.h
+++ 打印菱形.h
@@ -7,6 +7,8 @@ void Print(size_t n)   //n为要打印的行数
 	int i = 0;
 	for (i = 0; i <= n; i+=2)
 	{
+		for (size_t k = 0; k < (n - i - 1) / 2; ++k)   //打印*前面的空格，使菱形更美观
+			printf(" ");
 		for (size_t j = 0; j < i +1 ; ++j)
 		{
 			printf("*");
@@ -15,6 +17,8 @@ void Print(size_t n)   //n为要打印的行数
 	}
 	for (i = n - 2; i >0; i -= 2)
 	{
+		for (size_t k = 0; k < (n - i) / 2; ++k)   //打印*前面的空格，使菱形更美观
+			printf(" ");
 		for (size_t j = 0; j < i ; ++j)
 		{
 			printf("*");
