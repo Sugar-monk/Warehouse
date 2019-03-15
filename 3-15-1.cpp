a1cac330f68dbc14f92dd85ec52b0d4e2fef1ff1
suger-monk <1835868676@qq.com>
2019-03-15 23:11:46 +0800

    奇偶位置

---
+++ 3-15-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#include <string>
+#include<vector>
+
+class Solution {
+public:
+	void reOrderArray(vector<int> &array) {
+
+		int Lift = 0;
+		int right = array.size();
+		while ()
+
+	}
+};
