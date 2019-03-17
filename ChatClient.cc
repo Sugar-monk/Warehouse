c5356f4281743d8d9c480df880f1f95e03695ff4
suger-monk <1835868676@qq.com>
2019-03-17 22:16:09 +0800

    聊天客户端

---
+++ ChatClient.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include "ChatClient.hpp"
+
+static void Usage(std::string proc)
+{
+	std::cout << "Usage:" << proc << "peer_ip" << std::endl;
+}
+
+static void Menu(int &s)
+{
+	std::cout << "*************************************" << std::endl;
+	std::cout << "****  1.Register       2.Login   ****" << std::endl;
+	std::cout << "**                                 **" << std::endl;
+	std::cout << "****                   3.Exit    ****" << std::endl;
+	std::cout << "*************************************" << std::endl;
+	std::cout << "Placse Select->" ;
+	std::cin  >> s; 
+}
+
+
+int main(int argc, char *argv[])
+{
+	if(argc != 2)
+	{
+		Usage(argv[0]);
+		exit(1);
+	}
+	
+	int select = 0;
+	while(1)
+	{
+		Menu(select);
+		switch(select)
+		{
+			case 1:			//进行注册
+				cp->Register();
+				break;
+			case 2:			//进行登录\
+				if(cp->Login())
+				{
+					cp->Chat();
+				}
+				else
+				{
+					std::cout << "Login Failed!" << std::endl;
+				}
+				break;
+			case 3:
+				exit(0);
+				break;
+			default:
+				exit(1);
+				break;
+		}
+	}
+	ChatClient *cp = new ChatClient(argv[1]);
+	cp->InitClient();
+	if(cp->ConnectServer())												//链接服务器
+	{
+		std::cout<< "connect success" << std::endl;
+	}
+
+
+
+	return 0;
+}
